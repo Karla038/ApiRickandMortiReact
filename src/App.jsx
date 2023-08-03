72f20062e475c3ff2a1178fd7a6555bd6d7d4948
“Karla038” <karladk038@gmail.com>
2023-08-03 12:10:58 -0600

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
-import { useEffect, useState } from "react"
+import CharacterList from "./components/CharacterList"
+
 
 function App() {
 
-  const [characters, setCharacters] = useState([])
-
-  // Para hacer la petición a la api
-  useEffect(() =>{
-    async function fetchData() {
-      const response = await fetch('https://rickandmortyapi.com/api/character')
-      const data = await response.json()
-      setCharacters(data.results)
-    }
-
-    // Para llamar la función 
-    fetchData();
-  },[])
-
-  return <div>
-    <h1>Rick and Morty</h1>
-    {
-      //Con la función .map() se va a recorrer  la información de cada uno de los documentos
-      characters.map(character => {
-        return(
-          <div>
-            {/* Para poder obtener la información de los objetos del arreglo y en react se utilizan las llaves {} de esta forma puedes obtener la información */}
-            <h2>{character.name}</h2>
-            <img src={character.image} alt={character.name} />
-          </div>
-        )
-      })
-    }
+  return <div className="bg-dark text-white">
+    <h1 className="text-center display-1 py-4">Rick and Morty</h1>
+    <CharacterList />
+   
   </div>
 }
 
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://<Karla038.github.io/ApiRickandMortyReact/",
   plugins: [react()],
 })
